1388657e135b8751dbbcdc25e303dae9a8eec481
David Melendez <dave_melendez@hotmail.com>
2024-02-21 19:45:18 -0600

    https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.4

--- src/ACMESharp/Protocol/Resources/Authorization.cs
+++ src/ACMESharp/Protocol/Resources/Authorization.cs
@@ -1,10 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace ACMESharp.Protocol.Resources
 {
     /// <summary>
-    /// https://tools.ietf.org/html/draft-ietf-acme-acme-12#section-7.1.4
+    /// https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.4
     /// </summary>
     public class Authorization
     {
@@ -18,9 +18,11 @@ namespace ACMESharp.Protocol.Resources
 
         public string Expires { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
         [Required]
         public Challenge[] Challenges { get; set; }
 
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public bool? Wildcard { get; set; }
     }
 }
