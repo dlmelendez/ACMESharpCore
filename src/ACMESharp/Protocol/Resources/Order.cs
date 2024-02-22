ddfd23d2f5fbc7ab0eeb52b507d473ce827e36ee
David Melendez <dave_melendez@hotmail.com>
2024-02-21 19:46:03 -0600

    https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.3

--- src/ACMESharp/Protocol/Resources/Order.cs
+++ src/ACMESharp/Protocol/Resources/Order.cs
@@ -1,11 +1,10 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace ACMESharp.Protocol.Resources
 {
     /// <summary>
-    /// https://tools.ietf.org/html/draft-ietf-acme-acme-12#section-7.1.3
-    /// https://tools.ietf.org/html/draft-ietf-acme-acme-12#section-7.3
+    /// https://datatracker.ietf.org/doc/html/rfc8555#section-7.1.3
     /// </summary>
     public class Order
     {
@@ -14,14 +13,17 @@ namespace ACMESharp.Protocol.Resources
         public string Status { get; set; }
 
         public string Expires { get; set; }
+        
+        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+        [Required, MinLength(1)]
+        public Identifier[] Identifiers { get; set; }
 
         public string NotBefore { get; set; }
 
         public string NotAfter { get; set; }
 
-        [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
-        [Required, MinLength(1)]
-        public Identifier[] Identifiers { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Problem? Error { get; set; }
 
         [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
         [Required, MinLength(1)]
@@ -34,7 +36,5 @@ namespace ACMESharp.Protocol.Resources
         [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Certificate { get; set; }
 
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-        public Problem? Error { get; set; }
     }
 }
